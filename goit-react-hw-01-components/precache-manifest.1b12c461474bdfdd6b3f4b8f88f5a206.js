de55d1413eb1d02c76eb7471f0a5aba1e8e0b42c
Danil Kruhlikov <danylo.kruhlikov@nure.ua>
2020-09-28 14:52:39 +0300

    3 hw finished

---
+++ goit-react-hw-01-components/precache-manifest.1b12c461474bdfdd6b3f4b8f88f5a206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8b37c1f08624adcf22515b7fd215aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c66f33c8721748d39cca",
+    "url": "/static/css/main.8bac3af0.chunk.css"
+  },
+  {
+    "revision": "ca5e6355183a48637cf1",
+    "url": "/static/js/2.36101c65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36101c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66f33c8721748d39cca",
+    "url": "/static/js/main.438cd3b1.chunk.js"
+  },
+  {
+    "revision": "a93d14e41179cfc5c40c",
+    "url": "/static/js/runtime-main.4e1b0a06.js"
+  }
+]);
